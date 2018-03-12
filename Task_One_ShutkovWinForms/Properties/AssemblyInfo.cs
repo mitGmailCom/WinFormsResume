17c490438673ea42b5dba333b21ead63ad476b62
Admin <Admin@X>
2018-03-12 11:46:28 +0200

    Добавьте файлы проекта.

---
+++ Task_One_ShutkovWinForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Task_One_ShutkovWinForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Task_One_ShutkovWinForms")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("74f79eee-aef5-4b6e-9a7c-7f6a932c8e89")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Task_One_ShutkovWinForms/Resume.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_One_ShutkovWinForms
+{
+    public partial class Resume : Form
+    {
+        //      Вывести на экран свое(краткое!!!) резюме с помощью последовательности MessageBox’ов(числом не менее трех). Причем на заголовке
+        //      последнего должно отобразиться среднее число символов на странице (общее количество символов в резюме / количество MessageBox’ов).
+
+        public string str { get; private set; } = null;
+        public static int res { get; private set; } = 0;
+        
+        private static void Result(string tempStr)
+        {
+            res += tempStr.Length;
+
+        }
+
+        public Resume()
+        {
+            InitializeComponent();
+            str = "Резуме Студента Академии Шаг - Шутков Дмитрий Викторович...";
+            DialogResult result = MessageBox.Show(str, "РЕЗЮМЕ",
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+            Result(str);
+            if (result == DialogResult.OK)
+            {
+                str = "Дата рождения - 28.10.1984";
+                result = MessageBox.Show(str, "РЕЗЮМЕ", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                Result(str);
+                if (result == DialogResult.OK)
+                {
+                    str = "Контактный телефон - 097 - 984 - 76 - 36";
+                    result = MessageBox.Show(str, "РЕЗЮМЕ", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    Result(str);
+                    if (result == DialogResult.OK)
+                    {
+                        res = res / 4;
+                        result = MessageBox.Show("Всего доброго! Среднее количество символов = " + res.ToString(), "РЕЗЮМЕ", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+                    }
+                    else if (result == DialogResult.Cancel)
+                    {
+                        MessageBox.Show("Вы нажали кнопку Cancel");
+                    }
+                }
+                else if (result == DialogResult.Cancel)
+                {
+                    MessageBox.Show("Вы нажали кнопку Cancel");
+                }
+            }
+            else if (result == DialogResult.Cancel)
+            {
+                MessageBox.Show("Вы нажали кнопку Cancel");
+            }
+        }
+    }
+}
