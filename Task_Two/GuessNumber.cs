d5f79771ba2370b330e4e58462d15d0b266fc6d6
Admin <Admin@X>
2018-03-12 17:53:38 +0200

    Fix Range

--- Task_Two/GuessNumber.cs
+++ Task_Two/GuessNumber.cs
@@ -36,30 +36,48 @@ namespace Task_Two
         {
             do
             {
-                result = MessageBox.Show($"Если ??? > {number} -> YES/n??? < {number} -> NO", "Угадай число", MessageBoxButtons.YesNo);
-                if (result == DialogResult.Yes)
+                if (lastPointRange - firstPointRange == 0)
                 {
-                    firstPointRange = number;
-                    Rnd();
-                    countIter++;
-                    if (lastPointRange - firstPointRange == 1)
-                        flag = 1;
+                    number = lastPointRange;
+                    flag = 1;
                 }
-                if (result == DialogResult.No)
+                if (flag == 0)
                 {
-                    lastPointRange = number;
-                    Rnd();
-                    countIter++;
-                    if (lastPointRange - firstPointRange == 1)
+                    result = MessageBox.Show($"'???' > {number} --> YES\n'???' < {number} --> NO\n'???' = {number} --> CANCEL", "Угадай число", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+                    if (result == DialogResult.Yes)
+                    {
+                        firstPointRange = number + 1;
+                        Rnd();
+                        countIter++;
+                        //if (lastPointRange - firstPointRange == 1)
+                        //{
+                        //    flag = 1;
+                        //    number = lastPointRange - 1;
+                        //}
+                    }
+                    if (result == DialogResult.No)
+                    {
+                        lastPointRange = number - 1;
+                        Rnd();
+                        countIter++;
+                        //if (lastPointRange - firstPointRange == 1)
+                        //{
+                        //    number = lastPointRange - 1;
+                        //    flag = 1;
+                        //}
+                    }
+                    if (result == DialogResult.Cancel)
                         flag = 1;
+                    else
+                        flag = 0;
                 }
             } while (flag == 0);
-            result = MessageBox.Show($"{lastPointRange - 1}");
+            result = MessageBox.Show($"Вы загадали число - {number}");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            result = MessageBox.Show("Загадайте число от 1 до 2000!", "Угадай число", MessageBoxButtons.OK);
+            result = MessageBox.Show("Загадайте число от 1 до 2000!", "Угадай число", MessageBoxButtons.OK, MessageBoxIcon.Information);
             if (result == DialogResult.OK)
                 Rnd();
                 StartGame();
