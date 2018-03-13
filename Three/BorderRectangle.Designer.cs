12c4e0a49369a54de17ac1e15afce596d16752d4
Admin <Admin@X>
2018-03-13 11:53:15 +0200

    Add Progect Three(Border Rectangle)

---
+++ Three/BorderRectangle.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace Three
+{
+    partial class BorderRectangle
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BorderRectangle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "BorderRectangle";
+            this.Text = "BorderRectangle";
+            this.Load += new System.EventHandler(this.BorderRectangle_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.BorderRectangle_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.BorderRectangle_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Three/BorderRectangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Представьте,что у вас на форме есть прямоугольник, границы которого на 10 пикселей отстоят от границ рабочей области формы.Необходимо создать
+//следующие обработчики:
+//- Обработчик нажатия левой кнопки мыши,который выводит сообщение о том, где находится текущая точка: внутри прямоугольника, снаружи,на границе
+//прямоугольника. Домашнее задание №1 Если при нажатии левой кнопки мыши была нажата кнопка Control (Ctrl), то приложение должно закрываться;
+//- Обработчик нажатия правой кнопки мыши, который выводит в заголовок окна информацию о размере клиентской(рабочей) области окна в виде:
+//Ширина = x, Высота = y, где x и y – соответствующие параметры вышего окна;
+//- Обработчик перемещения указателя мыши в пределах рабочей области, который должен выводить в заголовок окна текущие координаты мыши x и y.
+
+namespace Three
+{
+    enum stateMouseCl {InRectangle = 1, OutRectangle = 2, OnRectangle = 3};
+    public partial class BorderRectangle : Form
+    {
+        DialogResult result;
+        public BorderRectangle()
+        {
+            InitializeComponent();
+        }
+
+        private void BorderRectangle_Load(object sender, EventArgs e)
+        { }
+
+        private void GetBorderRectangle(MouseEventArgs tempE)
+        {
+            int borderMaxX = this.ClientSize.Width - 10;
+            int borderMaxY = this.ClientSize.Height - 10;
+            int borderMinX = 10;
+            int borderMinY = 10;
+            if ((borderMaxX < tempE.X || borderMaxY < tempE.Y) || (borderMinX > tempE.X || borderMinY > tempE.Y))
+            {
+                result = MessageBox.Show($"{stateMouseCl.OutRectangle}", "Событие Click(Левая клавиша)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if ((borderMaxX > tempE.X & borderMinX < tempE.X & borderMaxY > tempE.Y & borderMinY < tempE.Y) || (borderMinX < tempE.X & borderMaxX > tempE.X & borderMinY < tempE.Y & borderMaxY > tempE.Y))
+            {
+                result = MessageBox.Show($"{stateMouseCl.InRectangle}", "Событие Click(Левая клавиша)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            if ((borderMaxX == tempE.X || borderMaxY == tempE.Y) || (borderMinX == tempE.X || borderMinY == tempE.Y))
+            {
+                result = MessageBox.Show($"{stateMouseCl.OnRectangle}", "Событие Click(Левая клавиша)", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+
+        private void BorderRectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = $"X = {MousePosition.X.ToString()} Y = {MousePosition.Y.ToString()}";
+        }
+
+        private void BorderRectangle_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                result = MessageBox.Show($"Размер клиентской области окна:\nШирина = {this.ClientSize.Width.ToString()}\nДлина = {this.ClientSize.Height.ToString()}");
+            }
+            if (e.Button == MouseButtons.Left)
+            {
+                GetBorderRectangle(e);
+            }
+        }
+    }
+}
